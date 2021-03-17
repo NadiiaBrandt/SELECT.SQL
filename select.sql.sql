7572f03d1c0e6fd1fa98246e48bd998ac35139c5
Nadiiabrandt <sakunaforever@gmail.com>
2021-03-17 17:25:39 +0200

    Uploading

---
+++ select.sql.sql
@@ -0,0 +1,37 @@
+USE AdventureWorks2019
+GO
+SELECT * 
+FROM HumanResources.Department 
+WHERE GroupName LIKE '%Research%' 
+ORDER BY DepartmentID DESC
+GO 
+
+USE AdventureWorks2019
+GO
+SELECT BusinessEntityID, JobTitle, BirthDate, Gender
+FROM HumanResources.Employee
+--WHERE NationalIDNumber>500000000 AND NationalIDNumber<1000000000
+WHERE NationalIDNumber BETWEEN 500000000 AND 1000000000
+GO
+
+USE AdventureWorks2019
+GO
+SELECT BusinessEntityID, JobTitle, BirthDate , Gender
+FROM HumanResources.Employee
+WHERE YEAR(BirthDate) IN ('1980','1990')
+GO
+
+USE AdventureWorks2019
+GO
+SELECT BusinessEntityID, ShiftID
+FROM HumanResources.EmployeeDepartmentHistory
+GROUP BY BusinessEntityID, ShiftID
+GO
+
+USE AdventureWorks2019
+GO
+SELECT BusinessEntityID, ShiftID
+FROM HumanResources.EmployeeDepartmentHistory
+GROUP BY BusinessEntityID, ShiftID
+HAVING COUNT(BusinessEntityID)>=2 AND COUNT(ShiftID)>=2
+GO
