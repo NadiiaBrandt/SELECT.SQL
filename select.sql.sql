2c2a8cbb6ca30cd46a6dc273ad4ec52455793008
NadiiaBrandt <78810966+NadiiaBrandt@users.noreply.github.com>
2021-03-18 19:22:01 +0200

    Cleaning

--- select.sql.sql
+++ select.sql.sql
@@ -4,33 +4,24 @@ SELECT *
 FROM HumanResources.Department 
 WHERE GroupName LIKE '%Research%' 
 ORDER BY DepartmentID DESC
-GO 
 
-USE AdventureWorks2019
 GO
 SELECT BusinessEntityID, JobTitle, BirthDate, Gender
 FROM HumanResources.Employee
 WHERE NationalIDNumber BETWEEN 500000000 AND 1000000000
-GO
 
-USE AdventureWorks2019
 GO
 SELECT BusinessEntityID, JobTitle, BirthDate , Gender
 FROM HumanResources.Employee
 WHERE YEAR(BirthDate) IN (1980,1990)
-GO
 
-USE AdventureWorks2019
 GO
 SELECT BusinessEntityID, ShiftID
 FROM HumanResources.EmployeeDepartmentHistory
 GROUP BY BusinessEntityID, ShiftID
-GO
 
-USE AdventureWorks2019
 GO
 SELECT BusinessEntityID, ShiftID
 FROM HumanResources.EmployeeDepartmentHistory
 GROUP BY BusinessEntityID, ShiftID
 HAVING COUNT(*)>=2
-GO
